be9525c942e16f9c641a1359f4d4983547f1370a
Rina Mito <satoshikibu@gmail.com>
2018-03-13 15:11:22 +0900

    第16回

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    # save
+    # @post = Post.new(params[:post])
+    # @post = Post.new(params.require[:post].permit(:title, :body))
+    @post = Post.new(post_params)
+    @post.save
+    # redirect
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
